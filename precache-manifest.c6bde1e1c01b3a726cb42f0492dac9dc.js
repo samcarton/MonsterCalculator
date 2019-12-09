383aa5d3301d97d74c6188210cded9c6a0458616
Sam Carton <sam169@gmail.com>
2019-12-10 00:07:05 +0800

    add build

---
+++ precache-manifest.c6bde1e1c01b3a726cb42f0492dac9dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a63b86248b2c64e787f85c8c5b6e5e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f557f730f6d51db8cc8e",
+    "url": "/static/css/main.76e27cc9.chunk.css"
+  },
+  {
+    "revision": "b5733f4b61ab01b32b28",
+    "url": "/static/js/2.1711de49.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.1711de49.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f557f730f6d51db8cc8e",
+    "url": "/static/js/main.206b90d3.chunk.js"
+  },
+  {
+    "revision": "8a0d75516d8ed624abc7",
+    "url": "/static/js/runtime-main.da59f130.js"
+  }
+]);
